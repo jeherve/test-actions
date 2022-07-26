49cf6e87ff873e681a9688a838ac2d6b62211a27
Jeremy Herve <jeremy@jeremy.hu>
2022-07-26 11:48:41 +0200

    Default Prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-const { setFailed, getInput, debug } = require( '@actions/core' );
-const { context, getOctokit } = require( '@actions/github' );
+const { setFailed, getInput, debug } = require("@actions/core");
+const { context, getOctokit } = require("@actions/github");
 
-( async function main() {
-	const token = getInput( 'github_token' );
-	if ( ! token ) {
-		setFailed( 'Input `github_token` is required' );
-		return;
-	}
+(async function main() {
+  const token = getInput("github_token");
+  if (!token) {
+    setFailed("Input `github_token` is required");
+    return;
+  }
 
-	// Get info about the event.
-	const { payload, eventName } = context;
+  // Get info about the event.
+  const { payload, eventName } = context;
 
-	// eslint-disable-next-line new-cap
-	const octokit = new getOctokit( token );
+  // eslint-disable-next-line new-cap
+  const octokit = new getOctokit(token);
 
-	debug( `Received event = '${ eventName }', action = '${ payload.action }'` );
+  debug(`Received event = '${eventName}', action = '${payload.action}'`);
 
-	// Trigger something when a Pull Request is opened.
-	if ( eventName === 'pull_request_target' && action === 'opened' ) {
-		debug( 'Pull Request opened' );
+  // Trigger something when a Pull Request is opened.
+  if (eventName === "pull_request_target" && action === "opened") {
+    debug("Pull Request opened");
 
-		const { number, repository } = payload;
-		const { owner, name } = repository;
+    const { number, repository } = payload;
+    const { owner, name } = repository;
 
+    debug(`Add a label to PR #${number}`);
+    await octokit.rest.issues.addLabels({
+      owner: owner.login,
+      repo: name,
+      issue_number: number,
+      labels: ["Just opened"],
+    });
+  }
 
-		debug( `Add a label to PR #${ number }` );
-		await octokit.rest.issues.addLabels({
-			owner: owner.login,
-			repo: name,
-			issue_number: number,
-			labels: ['Just opened'],
-		});
-	}
-
-	debug( 'Done.' );
-} )();
+  debug("Done.");
+})();
