c6588137ef375c8cecf082276f97d2d043a337ed
Jeremy Herve <jeremy@jeremy.hu>
2022-07-26 17:17:11 +0200

    Add a basic labeling logic

--- .github/actions/triage/src/index.js
+++ .github/actions/triage/src/index.js
@@ -1,6 +1,25 @@
 const { setFailed, getInput, debug } = require( '@actions/core' );
-const { context, getOctokit } = require( '@actions/github' );
+const { context, github } = require( '@actions/github' );
 
 ( async function main() {
 	debug( 'Our action is running' );
+
+	const token = getInput( 'github_token' );
+	if ( ! token ) {
+		setFailed( 'Input `github_token` is required' );
+		return;
+	}
+
+	// Get an instance of the Octokit client.
+	const octokit = github.getOctokit( token );
+
+	// Get info about the event.
+	const { payload: { number, repository: { owner, name } } } = context;
+
+	await octokit.rest.issues.addLabels( {
+		owner: owner.login,
+		repo: name,
+		issue_number: number,
+		labels: [ 'Issue triaged' ],
+	} );
 } )();
